e62c36fd2d1feafd6dcbb01c78759dcab281402e
Ilikethehuracan <95977153+Ilikethehuracan@users.noreply.github.com>
2022-04-19 08:50:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function sound() {
+    document.querySelector(".header1").innerHTML = "Ha, you made it worse!!!"
+    document.querySelector(".header2").style.display = "none"
+    document.querySelector(".rickRoll").play()
+}
